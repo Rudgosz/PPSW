1f36d1a4c4e87a88d71e1564ee442583e4963fe3
Rudgosz <tryton.maciek@gmail.com>
2026-02-15 18:21:05 +0100

    wysylanie z4

--- archive/wysylanie_lancuchow_znakowych/main.c
+++ archive/wysylanie_lancuchow_znakowych/main.c
@@ -1,13 +1,21 @@
 #include "uart.h"
+#include "string.h"
+#include "konwersje.h"
 
 int main() {
-    
+    unsigned int uiNumCounter = 0;
+    char cTextToSend[TRANSMITER_SIZE];
+
     UART_InitWithInt(9600);
-    
+
     while(1) {
+
         if(Transmiter_GetStatus() == FREE) {
-            Transmiter_SendString("test123");
             
+            CopyString("licznik 0x", cTextToSend);
+            UIntToHexStr(uiNumCounter, cTextToSend + 10);
+            Transmiter_SendString(cTextToSend);
+            uiNumCounter++;
         }
     }
 }
